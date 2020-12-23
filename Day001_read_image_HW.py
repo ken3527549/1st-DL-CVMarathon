40f105885462872f6354220b7e98157603783130
ken3527 <ken3527549@hotmail.com>
2020-12-23 22:52:55 +0800

    Day002_change_color_space_HW

---
+++ Day001_read_image_HW.py
@@ -0,0 +1,7 @@
+import cv2
+gray_img = cv2.imread('E:\cygwin\home\Ken-Destop\python\marathon\len_full.jpg',cv2.IMREAD_GRAYSCALE)
+rgb_img =  cv2.imread('E:\cygwin\home\Ken-Destop\python\marathon\len_full.jpg')
+cv2.imshow('gray_image', gray_img)
+cv2.imshow('rgb_image', rgb_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ Day002_change_color_space_HW.py
@@ -0,0 +1,13 @@
+import cv2
+img_gray = cv2.imread('E:\cygwin\home\Ken-Destop\python\marathon\len_full.jpg', cv2.IMREAD_GRAYSCALE)
+img_bgr = cv2.imread('E:\cygwin\home\Ken-Destop\python\marathon\len_full.jpg')
+img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
+img_hsl = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HLS)
+img_lab = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2LAB)
+cv2.imshow('img_gray', img_gray)
+cv2.imshow('img_bgr', img_bgr)
+cv2.imshow('img_hsv', img_hsv)
+cv2.imshow('img_hsl', img_hsl)
+cv2.imshow('img_lab', img_lab)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
